d22a32ca0f9421c3064501549e0452f26d4126de
Cody Price <dev.cprice@gmail.com>
2021-01-21 14:32:26 -0800

    Update icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,10 @@ export default function Home() {
       <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight">Boring Cars Detailing</h1>
       <Image
         alt="Boring Cars Detailing"
-        src="/assets/imgs/bcd_1920.png"
+        src="/assets/imgs/bcd_3840.png"
         priority
-        width="1920px"
-        height="1920px"
+        width="3840px"
+        height="3840px"
       />
       <h2 className="text-lg sm:text-3xl lg:text-3xl leading-none font-extrabold tracking-tight">Coming soon…</h2>
     </main>
