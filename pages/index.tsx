679b7a76277fbc9312b067359f5280033ed9781d
Cody Price <34115417+dev-cprice@users.noreply.github.com>
2021-01-21 14:41:22 -0800

    Update SEO (#1)
    * Update SEO
    
    * Update icon
    
    * Update icon size

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,12 @@ export const getStaticProps: GetStaticProps = async () => {
 export default function Home() {
   return (
     <main className="h-full bg-background text-gray-50 text-center p-16 mx-auto">
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight">Boring Cars Detailing</h1>
       <Image
         alt="Boring Cars Detailing"
-        src="/assets/imgs/bcd_1920.png"
+        src="/assets/imgs/bcd_xxl.png"
         priority
-        width="1920px"
-        height="1920px"
+        width="3694"
+        height="2506"
       />
       <h2 className="text-lg sm:text-3xl lg:text-3xl leading-none font-extrabold tracking-tight">Coming soon…</h2>
     </main>
