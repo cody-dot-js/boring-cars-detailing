b35f330a63a9a735bb010688e6274f51da24b946
Cody Price <dev.cprice@gmail.com>
2021-01-21 14:39:59 -0800

    Update icon size

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,12 @@ export const getStaticProps: GetStaticProps = async () => {
 export default function Home() {
   return (
     <main className="h-full bg-background text-gray-50 text-center p-16 mx-auto">
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight">Boring Cars Detailing</h1>
       <Image
         alt="Boring Cars Detailing"
-        src="/assets/imgs/bcd_3840.png"
+        src="/assets/imgs/bcd_xxl.png"
         priority
-        width="3840px"
-        height="3840px"
+        width="3694"
+        height="2506"
       />
       <h2 className="text-lg sm:text-3xl lg:text-3xl leading-none font-extrabold tracking-tight">Coming soon…</h2>
     </main>
