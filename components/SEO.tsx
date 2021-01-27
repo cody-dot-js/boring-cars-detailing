7f87caeef877110ba1c73603b9d6fbf09afa6f6c
Cody Price <dev.cprice@gmail.com>
2021-01-27 01:53:10 -0800

    add web manifest, update theme color

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -61,7 +61,7 @@ export function SEO(props: SEOProps) {
         href="/favicon-16x16.png"
       />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#db2577" />
-      <meta name="theme-color" content="#db2577" />
+      <meta name="theme-color" content="#ec4899" />
       <meta name="msapplication-TileColor" content="#db2577" />
     </Head>
   );
