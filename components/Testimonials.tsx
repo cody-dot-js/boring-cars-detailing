4ebf19b4c01aec675b4bf146b91d4ec6a3aa55c9
Cody Price <dev.cprice@gmail.com>
2021-01-28 03:56:26 -0800

    add initial testimonials

---
+++ components/Testimonials.tsx
@@ -0,0 +1,195 @@
+export function Testimonials() {
+  return (
+    <article>
+      <h2 className="relative z-40 text-center px-4 py-8 text-xl font-semibold tracking-wider text-cyan-300 uppercase">
+        Testimonials
+      </h2>
+      <section className="mt-8 overflow-hidden">
+        <div className="relative max-w-7xl mx-auto pt-20 pb-12 px-4 sm:px-6 lg:px-8 lg:py-20">
+          <svg
+            className="absolute top-full left-0 transform translate-x-80 -translate-y-24 lg:hidden"
+            width="784"
+            height="404"
+            fill="none"
+            viewBox="0 0 784 404"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="e56e3f81-d9c1-4b83-a3ba-0d0ac8c32f32"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  className="text-pink-900"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="784"
+              height="404"
+              fill="url(#e56e3f81-d9c1-4b83-a3ba-0d0ac8c32f32)"
+            />
+          </svg>
+
+          <svg
+            className="hidden lg:block absolute right-full top-1/2 transform translate-x-1/2 -translate-y-1/2"
+            width="404"
+            height="784"
+            fill="none"
+            viewBox="0 0 404 784"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="56409614-3d62-4985-9a10-7ca758a8f4f0"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  className="text-pink-900"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="784"
+              fill="url(#56409614-3d62-4985-9a10-7ca758a8f4f0)"
+            />
+          </svg>
+
+          <div className="relative lg:flex lg:items-center">
+            <div className="hidden lg:block lg:flex-shrink-0">
+              <img
+                className="h-64 w-64 rounded-full xl:h-80 xl:w-80"
+                src="/assets/imgs/testimonials/neil-canlas.jpg"
+                alt=""
+              />
+            </div>
+
+            <div className="relative lg:ml-10">
+              <svg
+                className="absolute top-0 left-0 transform -translate-x-8 -translate-y-24 h-36 w-36 text-pink-200 opacity-50"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 144 144"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-width="2"
+                  d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z"
+                />
+              </svg>
+              <blockquote className="relative">
+                <div className="text-2xl leading-9 font-medium text-gray-50">
+                  <p>
+                    <strong>Boring Cars Detailing</strong> worked on my amg
+                    gt53. Cem and his team are <strong>phenomenal</strong> and
+                    offer a <strong className="italic">second-to-none</strong>{" "}
+                    service! My car has never been in better hands.{" "}
+                    <strong className="italic text-pink-200">
+                      I highly recommend them
+                    </strong>{" "}
+                    and will continue to have them detail my car!
+                  </p>
+                </div>
+                <footer className="mt-8">
+                  <div className="flex">
+                    <div className="flex-shrink-0 lg:hidden">
+                      <img
+                        className="h-12 w-12 rounded-full"
+                        src="/assets/imgs/testimonials/neil-canlas.jpg"
+                        alt="Neil Canlas head shot"
+                        decoding="async"
+                      />
+                    </div>
+                    <div className="ml-4 lg:ml-0">
+                      <div className="text-base font-medium text-gray-50">
+                        Neil Canlas
+                      </div>
+                      <div className="text-base font-medium text-pink-300">
+                        Co-Founder, The Canlas Brothers Real Estate Group
+                      </div>
+                    </div>
+                  </div>
+                </footer>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="my-16 pb-16 bg-gradient-to-r from-teal-500 to-cyan-600 lg:pb-0 lg:z-10 lg:relative">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
+          <div className="relative lg:-my-8">
+            <div
+              aria-hidden="true"
+              className="absolute inset-x-0 top-0 h-1/2 bg-gradient-to-b from-gray-900 to-blueGray-900 lg:hidden"
+            />
+            <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
+              <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-10 lg:h-full">
+                <img
+                  className="object-cover lg:h-full lg:w-full"
+                  src="/assets/imgs/testimonials/matt-miesnieks.jpg"
+                  alt="Matt Miesnieks head shot"
+                  decoding="async"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
+            <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
+              <blockquote>
+                <div>
+                  <svg
+                    className="h-12 w-12 text-white opacity-25"
+                    fill="currentColor"
+                    viewBox="0 0 32 32"
+                    aria-hidden="true"
+                  >
+                    <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                  </svg>
+                  <p className="mt-6 text-2xl font-medium text-white">
+                    Cem's cleaned and detailed my cars; he is always{" "}
+                    <strong>super professional</strong> and{" "}
+                    <strong className="text-pink-200">
+                      does an amazing job
+                    </strong>
+                    ! My M3CS had a $5000 matte factory paint option which
+                    needed special care and it was handled perfectly.{" "}
+                    <strong className="italic text-pink-200">
+                      Very satisfied and ongoing customer!
+                    </strong>
+                  </p>
+                </div>
+                <footer className="mt-6">
+                  <p className="text-base font-medium text-white">
+                    Matt Miesnieks
+                  </p>
+                  <p className="text-base font-medium text-cyan-100">
+                    Niantic, Former CEO & Co-Founder at 6D.ai
+                  </p>
+                </footer>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  );
+}
