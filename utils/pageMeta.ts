32499d352c75defa1f255e8bb013026755b74e80
Cody Price <dev.cprice@gmail.com>
2021-02-10 14:41:37 -0800

    fix lint

--- utils/pageMeta.ts
+++ utils/pageMeta.ts
@@ -1,14 +1,11 @@
-import { createEnv, Env } from "hooks/useEnv";
 import { copyrightDate } from "./copyrightDate";
 
 export interface PageMeta {
   copyrightDate: string;
-  env: Env;
 }
 
 export function pageMeta(): PageMeta {
   return {
     copyrightDate: copyrightDate(),
-    env: createEnv(),
   };
 }
