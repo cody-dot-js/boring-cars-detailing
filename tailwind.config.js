8486efef44414809479d0cb0a6b5e359d654874c
Cody Price <34115417+dev-cprice@users.noreply.github.com>
2021-03-27 12:39:55 -0700

    performance improvements (#33)
    * performance improvements
    
    * trim woff/woff2 files down to only used chars
    * explicitly uppercase BORING in hero
    * update deps
    * explicitly list out tailwind purge patterns
    
    * modify purge options
    
    * add glitch to safelist
    
    * remove keyframes purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ const colors = require("tailwindcss/colors");
 const { laserwave, gray } = require("./theme/colors");
 
 module.exports = {
-  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "./pages/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
+      "./templates/**/*.{js,jsx,ts,tsx}",
+    ],
+  },
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
